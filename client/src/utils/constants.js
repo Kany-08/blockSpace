f93fa79c55dc8a2fc18af4d45495aa683287c973
Kany-08 <kanysharapova@gmail.com>
2022-02-22 11:03:36 +0100

    contract address added

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,4 @@
 import abi from './Transactions.json';
 
 export const contractABI = abi.abi;
-export const contractAddress = 'CONTRACT_ADDRESS';
+export const contractAddress = '0xb96B873f6F8CC8529a711ff99Eb951dDdF7835e6';
