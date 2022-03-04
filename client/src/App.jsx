88508e294e3835cf369063afb430a02541bbd1e7
Pascal Malangeri <s1l3ntz5@gmail.com>
2022-03-04 14:20:18 +0100

    earth background done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,26 @@
-import React from "react"
-import { Navbar, Welcome, Footer,Services, Transactions } from "./components"
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import CryptoTracker from "./Pages/CryptoTracker"
-import CoinPage from "./Pages/CoinPage"
-
+import React from "react";
+import {
+  Welcome,
+  Services,
+  Transactions,
+  LandingPage,
+} from "./components";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import CryptoTracker from "./Pages/CryptoTracker";
+import CoinPage from "./Pages/CoinPage";
 
 
 const App = () => {
-
   return (
-    
     <BrowserRouter>
-    <div className="min-h-screen">
-      <div className="gradient-bg-welcome">
-       <Navbar/>
-      
-        <Routes>  
-          <Route path='/' element={<CryptoTracker />} />
-          <Route path='/coins/:id' element={<CoinPage />} />
+      <div className="min-h-screen">
+        <div className="gradient-bg-welcome">
+          {/* <Navbar /> */}
+
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/crypto" element={<CryptoTracker />} />
+            <Route path='/coins/:id' element={<CoinPage />} />
           <Route path='/transact' element={
           <div>
             <Welcome/>
@@ -25,15 +28,13 @@ const App = () => {
             <Transactions/>
           </div>}
           />
-        </Routes>
-        
-        <Footer/>
-      </div>
-    </div>
-    </BrowserRouter>
-   
-    
-  )
-}
+          </Routes>
 
-export default App
+          {/* <Footer /> */}
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default App;
