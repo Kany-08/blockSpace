d0d68006ed7e9e64cea1193b4dde66c4cf401c07
Pascal Malangeri <s1l3ntz5@gmail.com>
2022-03-09 10:34:36 +0100

    earth changed

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -2,6 +2,7 @@ import React, {useContext } from 'react';
 import { AiFillPlayCircle } from 'react-icons/ai';
 import { SiEthereum } from 'react-icons/si';
 import { BsInfoCircle } from 'react-icons/bs';
+import { YoutubeEmbed } from "./Ytube";
 
 import { TransactionContext } from '../context/TransactionContext';
 import { Loader } from './';
@@ -40,7 +41,13 @@ const Welcome = () => {
     return (
         <div className='flex w-full justify-center items-center'>
             <div className='flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4'>
-                <div className='flex flex-1 justify-start flex-col mf:mr-10'>
+                    
+                    <div className="mr-150px">
+                        <YoutubeEmbed embedId="SSo_EIwHSd4" />
+                    </div>
+                
+                
+                {/* <div className='flex flex-1 justify-start flex-col mf:mr-10'>
                     <h1 className='text-3xl sm:text-5xl text-white text-gradient py-1'>
                         Send Crypto <br /> across the world
                     </h1>
@@ -81,10 +88,10 @@ const Welcome = () => {
                             Blockchain
                         </div>
                     </div>
-                </div>
+                </div> */}
 
                 <div className='flex flex-col flex-1 items-center justify-start w-full mf:mt-0 mt-10'>
-                    <div className='p-3 justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card white-glassmorphism'>
+                    {/* <div className='p-3 justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card white-glassmorphism'>
                         <div className='flex justify-between flex-col w-full h-full'>
                             <div className='flex justify-between items-start'>
                                 <div className='w-10 h-10 rounded-full border-2 border-white flex justify-center items-center'>
@@ -101,11 +108,11 @@ const Welcome = () => {
                                 </p>
                             </div>
                         </div>
-                    </div>
+                    </div> */}
 
 
                     <div className='p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism'>
-                        <Input placeholder="Address To" name="addressTo" type="text" handleChange={ handleChange } />
+                        <Input placeholder="Recipient" name="addressTo" type="text" handleChange={ handleChange } />
                         <Input placeholder="Amount (ETH)" name="amount" type="number" handleChange={ handleChange } />
                         <Input placeholder="Keyword (Gif)" name="keyword" type="text" handleChange={ handleChange } />
                         <Input placeholder="Enter Message" name="message" type="text" handleChange={ handleChange } />
