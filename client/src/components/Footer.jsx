1dc5b23faebe6e92b8100a2381851b1133f0dde2
Pascal Malangeri <s1l3ntz5@gmail.com>
2022-03-17 14:41:35 +0100

    quickfix

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -15,7 +15,6 @@ const size = {
 const MadeBy = styled.h3`
   color: #fff;
   position: relative;
-  margin-top: 2.45em;
   text-align: center;
   left: 50%;
   transform: translateX(-50%);
@@ -23,23 +22,19 @@ const MadeBy = styled.h3`
     display: none;
 }
 `;
-const icons = styled.div`{
-  display: flex;
-
-}`;
 
 
 const Footer = () => (
-  <div className="w-full md:justify-center justify-between items-center flex-col p-4" style={{height: "100%"}}>
+  <div className="w-full md:justify-center justify-between items-center flex-col p-12">
     <MadeBy>
         <u>Made By:</u> Hengameh Bolouri, Kanyshai Sharapova, Pascal Malangeri
       </MadeBy>
-    <div className="w-full flex sm:flex-row flex-col justify-between items-center my-4">
+    <div className="w-full flex sm:flex-row flex-col justify-between items-center">
     
       
-      <div className="flex flex-1 justify-evenly items-end flex-wrap sm:mt-0 mt-5 w-full">
+      <div className="flex flex-1 justify-center items-center flex-wrap w-full sm:pt-5 pb-4">
         
-          <p className="text-white pt-9 mx-2 cursor-pointer">
+          <p className="text-white mx-2 cursor-pointer">
             <FaTwitter size={20} />
           </p>
         
@@ -67,9 +62,9 @@ const Footer = () => (
       </p>
     </div>
 
-    <div className="sm:w-[90%] w-full h-[0.25px] bg-gray-400 mt-5 " />
+    <div className="sm:w-[100%] w-full h-[0.25px] bg-gray-400 mt-5 " />
 
-    <div className="sm:w-[90%] w-full flex justify-between items-center mt-3">
+    <div className="sm:w-[100%] w-full flex justify-between items-center mt-3">
       <p className="text-white text-left text-xs">@blockSpace2022</p>
       <p className="text-white text-right text-xs">All rights reserved</p>
     </div>
